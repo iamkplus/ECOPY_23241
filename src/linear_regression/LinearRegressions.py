08be184309bfd3b39afd70f888b65f04bfcbb2e4
iamkplus <iamkplus@gmail.com>
2023-11-15 20:11:28 +0100

    weekly-8

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -49,12 +49,10 @@ class LinearRegressionNP:
         beta = np.dot(np.dot(XTX_inv, self.right_hand_side.T), self.left_hand_side)
         residuals = self.left_hand_side - np.dot(self.right_hand_side, beta)
         sigma_squared = np.sum(residuals ** 2) / (len(self.left_hand_side) - self.right_hand_side.shape[1])
-
         se = np.sqrt(np.diagonal(sigma_squared * XTX_inv))
         t_stat = beta / se
         p_values = 2 * (1 - f.cdf(np.abs(t_stat), len(self.left_hand_side) - self.right_hand_side.shape[1],
                                   len(self.left_hand_side) - len(beta)))
-
         self._model = {'beta': beta, 'se': se, 't_stat': t_stat, 'p_values': p_values}
 
     def get_params(self):
@@ -67,8 +65,17 @@ class LinearRegressionNP:
         p_values = 2 * (1 - t.cdf(np.abs(t_stat), df))
         return pd.Series(p_values, name='P-values for the corresponding coefficients')
 
-    def get_wald_test_result(self, restrictions_matrix):
-        pass
+    def get_wald_test_result(self, R):
+        r = np.transpose(np.zeros((len(R))))
+        term_1 = np.dot(R, self._model['beta']) - r
+        term_2 = np.dot(np.dot(R, np.linalg.inv(np.dot(self.right_hand_side.T, self.right_hand_side))), np.transpose(R))
+        XTX_inv = np.linalg.inv(np.dot(self.right_hand_side.T, self.right_hand_side))
+        beta = np.dot(np.dot(XTX_inv, self.right_hand_side.T), self.left_hand_side)
+        residuals = self.left_hand_side - np.dot(self.right_hand_side, beta)
+        var = sigma_squared = np.sum(residuals ** 2) / (len(self.left_hand_side) - self.right_hand_side.shape[1])
+        wald = (np.dot(np.transpose(term_1), np.dot(np.linalg.inv(term_2), term_1))/len(R))/var
+        p_value = (1-f.cdf(wald, len(R), len(self.left_hand_side) - self.right_hand_side.shape[1]))
+        return f'Wald: {wald:.3f}, p-value: {p_value:.3f}'
 
     def get_model_goodness_values(self):
         y_mean = np.mean(self.left_hand_side)
@@ -78,5 +85,28 @@ class LinearRegressionNP:
         crs_denominator = sst
         crs = 1 - (ssr / crs_denominator)
         ars = 1 - (1 - crs) * (len(self.left_hand_side) - 1) / (len(self.left_hand_side) - self.right_hand_side.shape[1])
-
         return f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
+
+class LinearRegressionGLS:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+        self._model = None
+
+    def fit(self):
+        XTX_inv = np.linalg.inv(np.dot(self.right_hand_side.T, self.right_hand_side))
+        beta = np.dot(np.dot(XTX_inv, self.right_hand_side.T), self.left_hand_side)
+        residuals = self.left_hand_side - np.dot(self.right_hand_side, beta)
+        squared_residuals = residuals ** 2
+        sigma_squared = np.sum(residuals ** 2) / (len(self.left_hand_side) - self.right_hand_side.shape[1])
+
+        new_rhs = np.concatenate((np.log(squared_residuals), self.right_hand_side))
+        XTX_inv_new = np.linalg.inv(np.dot(new_rhs.T, new_rhs))
+        beta_feasible_gls = np.dot(np.dot(XTX_inv_new, new_rhs.T), np.log(squared_residuals))
+        v_inv_matrix = np.diag(1 / squared_residuals)
+        XTX_inv_gls = np.linalg.inv(np.dot(np.dot(new_rhs.T, v_inv_matrix), new_rhs))
+        beta_gls = np.dot(np.dot(XTX_inv_gls, new_rhs.T), np.log(squared_residuals))
+        self._model = {'beta_feasible_gls': beta}
+    def get_params(self):
+        params = self._model['beta_feasible_gls']
+        return pd.Series(params, name='Beta coefficients')
